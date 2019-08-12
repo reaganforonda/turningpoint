2dba876e65a70306505ca8a1e601ff2e8e7b7fbb
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 20:17:29 -0600

    registerform

--- src/Components/Landing/RegisterForm.js
+++ src/Components/Landing/RegisterForm.js
@@ -5,15 +5,31 @@ export default class RegisterForm extends React.Component{
         super(props);
 
         this.state={
-
+            email:'',
+            firstName: '',
+            lastName: '',
+            pw: '',
+            confirmPW:''
         }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name]: e.target.value});
     }
 
     render(){
         return (
-            <div>
-                
-            </div>
+            <form>
+                <div>
+                    <h2>Create An Account</h2>
+                </div>
+                <div>
+                    <input placeholder='Emai' name='email' type='email' value={this.state.email} 
+                        onChange={(e) => this.handleInputChange(e)} />
+                </div>
+            </form>
         )
     }
 }
