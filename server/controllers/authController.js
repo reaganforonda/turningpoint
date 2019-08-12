e8701523e905d664fde28b1ca91bce8f5e4c84a8
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 20:59:10 -0600

    authcontroller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,21 @@
-const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const util = require('../../src/utilities/utilities');
 
 module.exports = {
     register: async (req, res) => {
+        const db = req.app.get('db');
+        const {email, firstName, lastName, pw, confirmPW} = req.body;
+
+        if(pw !== confirmPW) {
+            res.sendStatus(400);
+        }
+
+        if(!util.validateEmail(email)) {
+            res.sendStatus(400);
+        }
+
         
+
+
     }
 }
