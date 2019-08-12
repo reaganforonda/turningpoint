114aea9d20f9814c5dcdac721d4386040f7ab605
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 20:09:52 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      {routes}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {routes}
+      </div>
+    );
+  }
 }
 
 export default App;
