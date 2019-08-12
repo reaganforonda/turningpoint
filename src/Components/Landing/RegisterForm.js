1f550b8ed55a0eeef59ae70a6adfa98d71341a59
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 20:32:18 -0600

    register form

--- src/Components/Landing/RegisterForm.js
+++ src/Components/Landing/RegisterForm.js
@@ -13,10 +13,21 @@ export default class RegisterForm extends React.Component{
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.resetForm = this.resetForm.bind(this);
     }
 
     handleInputChange(e) {
         this.setState({[e.target.name]: e.target.value});
+    }
+
+    resetForm(){
+        this.setState({
+            email:'',
+            firstName: '',
+            lastName: '',
+            pw: '',
+            confirmPW:''
+        })
     }
 
     render(){
@@ -26,7 +37,23 @@ export default class RegisterForm extends React.Component{
                     <h2>Create An Account</h2>
                 </div>
                 <div>
-                    <input placeholder='Emai' name='email' type='email' value={this.state.email} 
+                    <input placeholder='Email' name='email' type='email' value={this.state.email} 
+                        onChange={(e) => this.handleInputChange(e)} />
+                </div>
+                <div>
+                    <input placeholder='First Name' name='firstName' type='text' value={this.state.firstName} 
+                        onChange={(e) => this.handleInputChange(e)} />
+                </div>
+                <div>
+                    <input placeholder='Last Name' name='lastName' type='text' value={this.state.lastName} 
+                        onChange={(e) => this.handleInputChange(e)} />
+                </div>
+                <div>
+                    <input placeholder='Password' name='pw' type='password' value={this.state.pw} 
+                        onChange={(e) => this.handleInputChange(e)} />
+                </div>
+                <div>
+                    <input placeholder='Confirm Password' name='confirmPW' type='password' value={this.state.confirmPW} 
                         onChange={(e) => this.handleInputChange(e)} />
                 </div>
             </form>
