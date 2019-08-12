a32b11c77a420570902a86dfd49bc7b4cf822064
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 21:19:57 -0600

    register user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -14,7 +14,28 @@ module.exports = {
             res.sendStatus(400);
         }
 
-        
+        db.GET_EMAILS([email.toLowerCase()]).then((users) => {
+            if(users.length !== 0) {
+                if(users[0].email === email.toLowerCase()) {
+                    res.sendStatus(400);
+                } else {
+                    res.sendStatus(401);
+                }
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(confirmPW, salt);
+
+                db.REGISTER_USERS([email.toLowerCase(), firstName, lastName, hash]).then((result) => {
+                    res.sendStatus(200);
+                }).catch((err) => {
+                    console.log(`Server error while attempting to register user: ${err}`);
+                    res.sendStatus(500);
+                })
+            }
+        }).catch((err)=> {
+            console.log(`Error while attempting to get emials: ${err}`)
+            res.sendStatus(500);
+        });
 
 
     }
