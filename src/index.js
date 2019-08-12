f66216bf4908e03eeed9c908e69bfa29eff12e28
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 19:53:13 -0600

    router redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from 'store';
+import store from './store';
 
 ReactDOM.render(
     <Provider store={store}>
